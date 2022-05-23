9392e5c2f6176b6814a2b7bf7aec008e9eadac6f
AndrewLin2002-11-08 <andrewlin01@student.ciputra.ac.id>
2022-05-23 14:51:18 +0700

    adding mark2

---
+++ WindowsFormsApp11/mark 2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection sqlConnect = new MySqlConnection("server=localhost;uid=root;pwd=;database=premier_league");
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        String sqlQuery;
+        DataTable dtPemain = new DataTable();
+        int PosisiSekarang = 0;
+        int yeye = 9;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlQuery = sqlQuery = "select player.player_id, player.player_name, player.birthdate, nationality.nation, team.team_name as 'nama tim', player.team_number from player, nationality, team where player.nationality_id = nationality.nationality_id and player.team_id = team.team_id";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dtPemain);
+        }
+
+
+        public void IsiDataPemain(int Posisi)
+        {
+            tBox_id.Text = dtPemain.Rows[Posisi][0].ToString();
+            tBox_Player.Text = dtPemain.Rows[Posisi][1].ToString();
+            Tbox_nationality.Text = dtPemain.Rows[Posisi][2].ToString();
+            cBox_Team.DataSource = dtPemain;
+            cBox_Team.DisplayMember = "nama tim";
+            PosisiSekarang = Posisi;
+        }
+
+        private void Btn_last_Click(object sender, EventArgs e)
+        {
+            IsiDataPemain(dtPemain.Rows.Count - 1);
+        }
+
+        private void btn_first_Click(object sender, EventArgs e)
+        {
+            IsiDataPemain(0);
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+
+            if (PosisiSekarang < dtPemain.Rows.Count - 1)
+            {
+                PosisiSekarang++;
+                IsiDataPemain(PosisiSekarang);
+            }
+            else
+            {
+                MessageBox.Show("Data yang diinput sudah terakhir dan tidak bisa diinput lagi!");
+            }
+        }
+
+        private void cBox_Team_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //kosong
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+    }
+}
---
+++ WindowsFormsApp11/mark 2.designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_first = new System.Windows.Forms.Button();
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.Btn_last = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tBox_id = new System.Windows.Forms.TextBox();
+            this.tBox_Player = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Tbox_nationality = new System.Windows.Forms.TextBox();
+            this.cBox_Team = new System.Windows.Forms.ComboBox();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_first
+            // 
+            this.btn_first.Location = new System.Drawing.Point(34, 12);
+            this.btn_first.Name = "btn_first";
+            this.btn_first.Size = new System.Drawing.Size(75, 41);
+            this.btn_first.TabIndex = 0;
+            this.btn_first.Text = "<<";
+            this.btn_first.UseVisualStyleBackColor = true;
+            this.btn_first.Click += new System.EventHandler(this.btn_first_Click);
+            // 
+            // btn_prev
+            // 
+            this.btn_prev.Location = new System.Drawing.Point(128, 12);
+            this.btn_prev.Name = "btn_prev";
+            this.btn_prev.Size = new System.Drawing.Size(75, 41);
+            this.btn_prev.TabIndex = 1;
+            this.btn_prev.Text = "<";
+            this.btn_prev.UseVisualStyleBackColor = true;
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(230, 12);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(75, 41);
+            this.btn_next.TabIndex = 2;
+            this.btn_next.Text = ">";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // Btn_last
+            // 
+            this.Btn_last.Location = new System.Drawing.Point(333, 12);
+            this.Btn_last.Name = "Btn_last";
+            this.Btn_last.Size = new System.Drawing.Size(75, 41);
+            this.Btn_last.TabIndex = 3;
+            this.Btn_last.Text = ">>";
+            this.Btn_last.UseVisualStyleBackColor = true;
+            this.Btn_last.Click += new System.EventHandler(this.Btn_last_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Player ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Player Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "BirthDate";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nationality";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 259);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Team";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(41, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Team Number";
+            // 
+            // tBox_id
+            // 
+            this.tBox_id.Location = new System.Drawing.Point(128, 76);
+            this.tBox_id.Name = "tBox_id";
+            this.tBox_id.Size = new System.Drawing.Size(439, 26);
+            this.tBox_id.TabIndex = 10;
+            // 
+            // tBox_Player
+            // 
+            this.tBox_Player.Location = new System.Drawing.Point(145, 124);
+            this.tBox_Player.Name = "tBox_Player";
+            this.tBox_Player.Size = new System.Drawing.Size(439, 26);
+            this.tBox_Player.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(128, 165);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(282, 26);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // Tbox_nationality
+            // 
+            this.Tbox_nationality.Location = new System.Drawing.Point(128, 212);
+            this.Tbox_nationality.Name = "Tbox_nationality";
+            this.Tbox_nationality.Size = new System.Drawing.Size(439, 26);
+            this.Tbox_nationality.TabIndex = 13;
+            // 
+            // cBox_Team
+            // 
+            this.cBox_Team.FormattingEnabled = true;
+            this.cBox_Team.Location = new System.Drawing.Point(108, 256);
+            this.cBox_Team.Name = "cBox_Team";
+            this.cBox_Team.Size = new System.Drawing.Size(241, 28);
+            this.cBox_Team.TabIndex = 14;
+            this.cBox_Team.SelectedIndexChanged += new System.EventHandler(this.cBox_Team_SelectedIndexChanged);
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(45, 347);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(96, 36);
+            this.btn_Save.TabIndex = 15;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Location = new System.Drawing.Point(161, 347);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(119, 36);
+            this.btn_Cancel.TabIndex = 16;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(173, 296);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(211, 26);
+            this.numericUpDown1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 510);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.cBox_Team);
+            this.Controls.Add(this.Tbox_nationality);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.tBox_Player);
+            this.Controls.Add(this.tBox_id);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_last);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.btn_prev);
+            this.Controls.Add(this.btn_first);
+            this.Name = "Form1";
+            this.Text = "Form Pemain";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_first;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button Btn_last;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tBox_id;
+        private System.Windows.Forms.TextBox tBox_Player;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox Tbox_nationality;
+        private System.Windows.Forms.ComboBox cBox_Team;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
